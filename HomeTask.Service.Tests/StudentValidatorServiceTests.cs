9dd06e1e9afc099901ddc72731039954457a19f8
jcrm86 <jcrm86@gmail.com>
2023-07-22 14:56:58 -0500

    StudentValidatorService UT Tests ok

--- HomeTask.Service.Tests/StudentValidatorServiceTests.cs
+++ HomeTask.Service.Tests/StudentValidatorServiceTests.cs
@@ -1,14 +1,8 @@
 ﻿using HomeTask.Data.Interfaces;
-using NUnit.Framework;
-using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HomeTask.Data;
 using HomeTask.Data.Models;
 using HomeTask.Utils;
+using Moq;
+using NUnit.Framework;
 using System.Net;
 
 namespace HomeTask.Service.Tests
@@ -50,5 +44,59 @@ namespace HomeTask.Service.Tests
             Assert.That(result.Message, Is.EqualTo(fakeResult.Message));
         }
 
+        [Test]
+        public void Student_Password_Validation_Succeded()
+        {
+            //Arrange
+            var fakeStudent = new Student()
+            {
+                StudentId = 1,
+                Name = "Juan",
+                Password = "password1",
+                State = "Active"
+            };
+
+            var fakeResult = new Result<Student>((int)HttpStatusCode.OK, "Fake good response");
+            fakeResult.Data = fakeStudent;
+
+            _mockedStudentData.Setup(x => x.GetStudentData(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakeResult);
+
+            //Act
+            var result = _systemUnderTest.StudentPasswordValidation(1, "password1", It.IsAny<CancellationToken>()).Result;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.HttpStatusCode, Is.EqualTo(fakeResult.HttpStatusCode));
+            Assert.That(result.Message, Is.EqualTo("Student authenticated ok"));
+            Assert.IsNotNull(result.Data);
+            Assert.That(result.Data.Name, Is.EqualTo(fakeResult.Data.Name));
+            Assert.That(result.Data.StudentId, Is.EqualTo(fakeResult.Data.StudentId));
+        }
+
+        [Test]
+        public void Student_Password_Validation_Failed_Because_Different_PAsswords()
+        {
+            //Arrange
+            var fakeStudent = new Student()
+            {
+                StudentId = 1,
+                Name = "Juan",
+                Password = "password1",
+                State = "Active"
+            };
+
+            var fakeResult = new Result<Student>((int)HttpStatusCode.OK, "Fake good response");
+            fakeResult.Data = fakeStudent;
+
+            _mockedStudentData.Setup(x => x.GetStudentData(It.IsAny<int>(), It.IsAny<CancellationToken>())).ReturnsAsync(fakeResult);
+
+            //Act
+            var result = _systemUnderTest.StudentPasswordValidation(1, "12345", It.IsAny<CancellationToken>()).Result;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.That(result.HttpStatusCode, Is.EqualTo((int)HttpStatusCode.Unauthorized));
+            Assert.That(result.Message, Is.EqualTo("Student authentication failed"));
+        }
     }
 }
